4998cf33e35a895bf68189a4ed155d54de857c5a
nickcen <cenyongh@gmail.com>
2022-09-15 13:12:51 +0800

    update persist and restore mechanism

--- ego/infra/ego_file/actor/src/lib.rs
+++ ego/infra/ego_file/actor/src/lib.rs
@@ -1,21 +1,30 @@
-use candid::{candid_method};
-
-use ic_cdk::caller;
+use candid::{candid_method, Encode, Decode};
+use ic_cdk::export::candid::{CandidType, Deserialize};
+use ic_cdk::{caller, trap};
 use ic_cdk_macros::*;
 
 use ego_file_mod::service::{EgoFileService};
 use ego_file_mod::state::STORAGE;
-use ego_file_mod::types::{FileMainReadRequest, FileMainReadResponse, FileMainWriteRequest, FileMainWriteResponse};
+use ego_file_mod::types::{EgoFileError, FileMainReadRequest, FileMainReadResponse, FileMainWriteRequest, FileMainWriteResponse};
 use ego_utils::types::EgoError;
-
-
+use ic_cdk::api::stable::{stable64_grow, stable64_read, stable64_write};
+use ego_file_mod::storage::{DEFAULT_FILE_SIZE, Storage, HEADER_SIZE, WASM_PAGE_SIZE};
 use ego_users::inject_ego_users;
 
+
+inject_ego_users!();
 
 #[init]
 #[candid_method(init)]
 fn init() {
     ic_cdk::println!("ego-file: init, caller is {}", caller());
+
+    ic_cdk::println!("==> create stable page for state");
+    let pages_to_grow = HEADER_SIZE / WASM_PAGE_SIZE;
+    let result = stable64_grow(pages_to_grow);
+    if result.is_err() {
+        trap(&format!("failed to grow stable memory by {} pages", pages_to_grow))
+    }
 }
 
 /********************  file method ********************/
@@ -38,33 +47,68 @@ fn file_main_read(req: FileMainReadRequest) -> Result<FileMainReadResponse, EgoE
     Ok(FileMainReadResponse {data})
 }
 
+#[derive(CandidType, Deserialize)]
+struct StableState {
+    storage: Storage,
+    user: User,
+}
+
 #[pre_upgrade]
 fn pre_upgrade() {
     ic_cdk::println!("ego-file: pre_upgrade");
-
-    STORAGE.with(|s| {
-        USER.with(|u| {
-            s.borrow_mut().header.user = u.borrow().clone();
-        });
-        s.borrow().persist()
-    })
 }
 
 #[post_upgrade]
 fn post_upgrade() {
     ic_cdk::println!("ego-file: post_upgrade");
-
-    STORAGE.with(|s| {
-        match s.borrow().restore() {
-            Some(storage) => {
-                *s.borrow_mut() = storage;
-                USER.with(|u| {
-                   *u.borrow_mut() = s.borrow().header.user.clone();
-                });
-            },
-            _ => {}
-        }
-    })
 }
 
-inject_ego_users!();
+/********************  persist method ********************/
+#[update(name = "state_persist")]
+#[candid_method(update, rename = "state_persist")]
+fn state_persist() -> Result<bool, EgoError> {
+    ic_cdk::println!("ego-file: state_persist");
+
+    let storage = STORAGE.with(|s| s.borrow().clone());
+    let user = USER.with(|u| u.borrow().clone());
+
+    let state = StableState {
+        storage, user
+    };
+
+
+    let data = Encode!(&state).unwrap();
+
+    ic_cdk::println!("==> data length is: {}", data.len());
+
+    if data.len() > DEFAULT_FILE_SIZE as usize {
+        Err(EgoFileError::UnknownError("state too large".to_string()).into())
+    } else {
+        stable64_write(0, &(data.len() as u64).to_le_bytes()); // file length
+        stable64_write(8, &data);
+
+        Ok(true)
+    }
+}
+
+#[update(name = "state_restore")]
+#[candid_method(update, rename = "state_restore")]
+fn state_restore() -> Result<bool, EgoError> {
+    ic_cdk::println!("ego-file: state_restore");
+
+    // read file
+    let mut buf = vec![0; DEFAULT_FILE_SIZE as usize];
+    stable64_read(0, &mut buf); // file length
+    let len = u64::from_le_bytes(buf[0..8].try_into().unwrap()) as usize;
+
+    ic_cdk::println!("==> data length is: {}", len);
+
+    let data = &buf[8..8 + len];
+    let state = Decode!(data, StableState).unwrap();
+
+    STORAGE.with(|s| *s.borrow_mut() = state.storage);
+    USER.with(|u| *u.borrow_mut() = state.user);
+
+    Ok(true)
+}
+
