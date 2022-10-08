057f80a14da90d63617c82aa63231d5e503cad87
nickcen <cenyongh@gmail.com>
2022-10-08 11:29:14 +0800

    unit test for ego_dev service call back

---
+++ ego/infra/ego_store/mod/tests/ego_dev_service_test.rs
@@ -0,0 +1,89 @@
+use ic_cdk::export::Principal;
+
+use ego_store_mod::service::EgoStoreService;
+use ego_store_mod::state::EGO_STORE;
+use ego_store_mod::types::QueryParam;
+use ego_types::app::{App, Category, Wasm};
+use ego_types::app::CanisterType::{ASSET, BACKEND};
+use ego_types::version::Version;
+
+static FILE_CANISTER_ID: &str = "amybd-zyaaa-aaaah-qc4hq-cai";
+
+static EXISTS_APP_ID: &str = "app_test";
+static EXISTS_APP_NAME: &str = "test app";
+static EXISTS_APP_LOGO: &str = "logo";
+static EXISTS_APP_DESCRIPTION: &str = "test is app description";
+
+static NEW_APP_ID: &str = "new_app";
+static NEW_APP_NAME: &str = "new_app";
+static NEW_APP_LOGO: &str = "new_app logo";
+static NEW_APP_DESCRIPTION: &str = "test is new app description";
+
+
+pub fn set_up() {
+  let file_canister = Principal::from_text(FILE_CANISTER_ID.to_string()).unwrap();
+
+  let version = Version::new(1, 0, 0);
+
+  EGO_STORE.with(|ego_store| {
+    // add app
+    let frontend = Wasm::new(EXISTS_APP_ID.to_string(), version, ASSET, None);
+    let backend = Wasm::new(EXISTS_APP_ID.to_string(), version, BACKEND, Some(file_canister));
+    let app = App::new(EXISTS_APP_ID.to_string(), EXISTS_APP_NAME.to_string(), Category::Vault, EXISTS_APP_LOGO.to_string(), EXISTS_APP_DESCRIPTION.to_string(), version, frontend, backend, 1.2f32);
+    ego_store.borrow_mut().apps.insert(EXISTS_APP_ID.to_string(), app);
+  });
+}
+
+
+#[test]
+fn app_main_release_new_app() {
+  set_up();
+
+  // before new app release
+  let apps = EgoStoreService::app_main_list(QueryParam::ByCategory { category: Category::Vault }).unwrap();
+  assert_eq!(1, apps.len());
+
+  // add app
+  let file_canister = Principal::from_text(FILE_CANISTER_ID.to_string()).unwrap();
+  let version = Version::new(1, 0, 0);
+
+  let frontend = Wasm::new(NEW_APP_ID.to_string(), version, ASSET, None);
+  let backend = Wasm::new(NEW_APP_ID.to_string(), version, BACKEND, Some(file_canister));
+  let app = App::new(NEW_APP_ID.to_string(), NEW_APP_NAME.to_string(), Category::Vault, NEW_APP_LOGO.to_string(), NEW_APP_DESCRIPTION.to_string(), version, frontend, backend, 1.2f32);
+
+  let result = EgoStoreService::app_main_release(app);
+  assert!(result.is_ok());
+
+  let apps = EgoStoreService::app_main_list(QueryParam::ByCategory { category: Category::Vault }).unwrap();
+  assert_eq!(2, apps.len());
+
+  let app = EgoStoreService::app_main_get(NEW_APP_ID.to_string()).unwrap();
+  assert_eq!(NEW_APP_NAME.to_string(), app.name);
+}
+
+#[test]
+fn app_main_release_new_app_version() {
+  set_up();
+
+  // before new app release
+  let apps = EgoStoreService::app_main_list(QueryParam::ByCategory { category: Category::Vault }).unwrap();
+  assert_eq!(1, apps.len());
+
+  // add app
+  let file_canister = Principal::from_text(FILE_CANISTER_ID.to_string()).unwrap();
+  let version = Version::new(1, 0, 1);
+
+  let frontend = Wasm::new(EXISTS_APP_ID.to_string(), version, ASSET, None);
+  let backend = Wasm::new(EXISTS_APP_ID.to_string(), version, BACKEND, Some(file_canister));
+  let app = App::new(EXISTS_APP_ID.to_string(), EXISTS_APP_NAME.to_string(), Category::Vault, EXISTS_APP_LOGO.to_string(), EXISTS_APP_DESCRIPTION.to_string(), version, frontend, backend, 1.2f32);
+
+  let result = EgoStoreService::app_main_release(app);
+  assert!(result.is_ok());
+
+  let apps = EgoStoreService::app_main_list(QueryParam::ByCategory { category: Category::Vault }).unwrap();
+  assert_eq!(1, apps.len());
+
+  let app = EgoStoreService::app_main_get(EXISTS_APP_ID.to_string()).unwrap();
+  assert_eq!(EXISTS_APP_NAME.to_string(), app.name);
+  assert_eq!(version, app.current_version);
+}
