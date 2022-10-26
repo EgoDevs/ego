485d9ff62291d40707495dd41357200d7a537525
nickcen <cenyongh@gmail.com>
2022-10-27 00:15:52 +0800

    create ego_log canister

--- ego/infra/ego_log/actor/src/lib.rs
+++ ego/infra/ego_log/actor/src/lib.rs
@@ -1,86 +1,95 @@
-use ic_cdk::export::candid::{CandidType, Deserialize};
-use serde::Serialize;
 use candid::candid_method;
-use ic_cdk::{storage};
 use ic_cdk::api::time;
+use ic_cdk::export::candid::{CandidType, Deserialize};
+use ic_cdk::storage;
+use serde::Serialize;
 
-use ic_cdk_macros::*;
 use ego_log_mod::ego_log::EgoLog;
 use ego_log_mod::log::Log;
-use ego_log_mod::state::{EGO_LOG};
+use ego_log_mod::state::EGO_LOG;
+use ic_cdk_macros::*;
 
-use ego_users::inject_ego_users;
 use ego_macros::inject_balance_get;
+use ego_users::inject_ego_users;
+use ego_registry::inject_ego_registry;
 
 inject_balance_get!();
 inject_ego_users!();
+inject_ego_registry!();
 
 #[derive(Clone, Debug, CandidType, Deserialize)]
 pub struct InitArg {
-  init_caller: Option<Principal>,
+    init_caller: Option<Principal>,
 }
 
 #[init]
 #[candid_method(init)]
 pub fn init(arg: InitArg) {
-  let caller = arg.init_caller.unwrap_or(caller());
-  ic_cdk::println!("ego-log: init, caller is {}", caller.clone());
+    let caller = arg.init_caller.unwrap_or(caller());
+    ic_cdk::println!("ego-log: init, caller is {}", caller.clone());
 
-  ic_cdk::println!("==> add caller as the owner");
-  users_init(caller.clone());
+    ic_cdk::println!("==> add caller as the owner");
+    users_init(caller.clone());
 }
-
 
 #[derive(CandidType, Deserialize, Serialize)]
 struct PersistState {
-  pub ego_log: EgoLog,
-  pub user: User
+    pub ego_log: EgoLog,
+    pub user: User,
+    pub registry: Registry
 }
 
 #[pre_upgrade]
 fn pre_upgrade() {
-  ic_cdk::println!("ego-log: pre_upgrade");
-  let ego_log = EGO_LOG.with(|ego_log| ego_log.borrow().clone());
-  let user = users_pre_upgrade();
+    ic_cdk::println!("ego-log: pre_upgrade");
+    let ego_log = EGO_LOG.with(|ego_log| ego_log.borrow().clone());
+    let user = users_pre_upgrade();
+    let registry = registry_pre_upgrade();
 
-  let state = PersistState { ego_log, user };
-  storage::stable_save((state, )).unwrap();
+    let state = PersistState { ego_log, user, registry };
+    storage::stable_save((state,)).unwrap();
 }
 
 #[post_upgrade]
 fn post_upgrade() {
-  ic_cdk::println!("ego-log: post_upgrade");
-  let (state, ): (PersistState, ) = storage::stable_restore().unwrap();
-  EGO_LOG.with(|ego_log|
-    *ego_log.borrow_mut() = state.ego_log
-  );
+    ic_cdk::println!("ego-log: post_upgrade");
+    let (state,): (PersistState,) = storage::stable_restore().unwrap();
+    EGO_LOG.with(|ego_log| *ego_log.borrow_mut() = state.ego_log);
 
-  users_post_upgrade(state.user);
+    users_post_upgrade(state.user);
+    registry_post_upgrade(state.registry);
 }
+
+/********************  methods for ego_registry   ********************/
+fn on_canister_added(name: &str, canister_id: Principal) {
+    let _ = match name {
+        _ => role_user_add(canister_id).unwrap()
+    };
+}
+
 
 /********************  user  ********************/
 #[update(name = "canister_log_add", guard = "user_guard")]
 #[candid_method(update, rename = "canister_log_add")]
 fn canister_log_add(message: String) {
-  ic_cdk::println!("ego-log: canister_log_add");
-  EGO_LOG.with(|ego_log| {
-    ego_log.borrow_mut().canister_log_add(caller(), time(), message)
-  });
+    ic_cdk::println!("ego-log: canister_log_add");
+    EGO_LOG.with(|ego_log| {
+        ego_log
+            .borrow_mut()
+            .canister_log_add(caller(), time(), message)
+    });
 }
 
 #[query(name = "canister_log_get", guard = "user_guard")]
 #[candid_method(query, rename = "canister_log_get")]
-fn canister_log_get(from_ts: u64, to_ts: u64) -> Vec<Log>{
-  ic_cdk::println!("ego-log: canister_log_get");
-  EGO_LOG.with(|ego_log| {
-    ego_log.borrow().canister_log_get(from_ts, to_ts)
-  })
+fn canister_log_get(from_ts: u64, to_ts: u64) -> Vec<Log> {
+    ic_cdk::println!("ego-log: canister_log_get");
+    EGO_LOG.with(|ego_log| ego_log.borrow().canister_log_get(from_ts, to_ts))
 }
 
 /********************  notify  ********************/
 #[update(name = "message_main_notify", guard = "user_guard")]
 #[candid_method(update, rename = "message_main_notify")]
 async fn message_main_notify() {
-  ic_cdk::println!("ego-log: message_main_notify");
-
+    ic_cdk::println!("ego-log: message_main_notify");
 }
--- ego/infra/ego_log/actor/src/main.rs
+++ ego/infra/ego_log/actor/src/main.rs
@@ -7,9 +7,9 @@ fn main() {}
 #[allow(dead_code)]
 #[cfg(not(any(target_arch = "wasm32", test)))]
 fn main() {
-    use ic_cdk::export::Principal;
-    use ego_log_mod::log::Log;
     use crate::lib::InitArg;
+    use ego_log_mod::log::Log;
+    use ic_cdk::export::Principal;
     candid::export_service!();
     std::print!("{}", __export_service());
 }
