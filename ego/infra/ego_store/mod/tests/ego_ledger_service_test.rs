4df7f7df83a6200c2608146e4dfa37c5277ce7ff
邱春季 <qiuchunji01@chinatopcredit.com>
2022-11-15 15:47:07 +0800

    update ego store test case

--- ego/infra/ego_store/mod/tests/ego_ledger_service_test.rs
+++ ego/infra/ego_store/mod/tests/ego_ledger_service_test.rs
@@ -16,7 +16,7 @@ static EXISTS_USER_ID: &str = "225da-yaaaa-aaaah-qahrq-cai";
 static EXISTS_TENANT_ID: &str = "22ayq-aiaaa-aaaai-qgmma-cai";
 static TEST_WALLET_ID: &str = "5vreg-2yaaa-aaaaf-ajkdq-cai";
 
-mock!{
+mock! {
   Ledger{}
 
   impl TEgoLedger for Ledger{
@@ -25,133 +25,165 @@ mock!{
 }
 
 pub fn set_up() {
-  let tenant_principal = Principal::from_text(EXISTS_TENANT_ID.to_string()).unwrap();
-  let wallet_principal = Principal::from_text(EXISTS_WALLET_ID.to_string()).unwrap();
-  let user_principal = Principal::from_text(EXISTS_USER_ID.to_string()).unwrap();
-  let store_principal = Principal::from_text(STORE_ID.to_string()).unwrap();
+    let tenant_principal = Principal::from_text(EXISTS_TENANT_ID.to_string()).unwrap();
+    let wallet_principal = Principal::from_text(EXISTS_WALLET_ID.to_string()).unwrap();
+    let user_principal = Principal::from_text(EXISTS_USER_ID.to_string()).unwrap();
+    let store_principal = Principal::from_text(STORE_ID.to_string()).unwrap();
 
-  EGO_STORE.with(|ego_store| {
-    // add wallet
-    let mut wallet = Wallet::new(tenant_principal, wallet_principal, user_principal);
+    EGO_STORE.with(|ego_store| {
+        // add wallet
+        let mut wallet = Wallet::new(tenant_principal, wallet_principal, user_principal);
 
-    // add order
-    let order = Order::new(wallet_principal, &store_principal, 1.2f32, 10);
-    ego_store.borrow_mut().orders.insert(Memo(10), order);
+        // add order
+        let order = Order::new(wallet_principal, &store_principal, 1.2f32, 10);
+        ego_store.borrow_mut().orders.insert(Memo(10), order);
 
-    wallet.orders.push(Memo(10));
+        wallet.orders.push(Memo(10));
 
-    ego_store.borrow_mut().wallets.insert(wallet_principal, wallet);
-  });
+        ego_store
+            .borrow_mut()
+            .wallets
+            .insert(wallet_principal, wallet);
+    });
 }
 
 #[test]
 fn wallet_order_new() {
-  set_up();
+    set_up();
 
-  let exist_wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
-  let store_id = Principal::from_text(STORE_ID).unwrap();
+    let exist_wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
+    let store_id = Principal::from_text(STORE_ID).unwrap();
 
-  // get order list before make order
-  let result = EgoStoreService::wallet_order_list(exist_wallet_id);
-  assert!(result.is_ok());
-  assert_eq!(1, result.unwrap().len());
+    // get order list before make order
+    let result = EgoStoreService::wallet_order_list(exist_wallet_id);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().len());
 
-  // create order
-  let mut ego_ledger = MockLedger::new();
-  ego_ledger 
-    .expect_ledger_payment_add()
-    .returning(|_| ());
+    // create order
+    let mut ego_ledger = MockLedger::new();
+    ego_ledger.expect_ledger_payment_add().returning(|_| ());
 
-  let result = EgoStoreService::wallet_order_new(ego_ledger,exist_wallet_id, store_id, 1.2);
-  assert!(result.is_ok());
-  assert_eq!(1, result.unwrap().memo.0);
+    let result = EgoStoreService::wallet_order_new(ego_ledger, exist_wallet_id, store_id, 1.2);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().memo.0);
 
-  // get order list after make order
-  let result = EgoStoreService::wallet_order_list(exist_wallet_id);
-  assert!(result.is_ok());
-  assert_eq!(2, result.unwrap().len());
+    // get order list after make order
+    let result = EgoStoreService::wallet_order_list(exist_wallet_id);
+    assert!(result.is_ok());
+    assert_eq!(2, result.unwrap().len());
 }
 
 #[test]
 fn wallet_order_notify() {
-  set_up();
+    set_up();
 
-  let ledger_principal = Principal::from_text(LEDGER_ID.to_string()).unwrap();
-  let exist_wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
-  // get order list before make order
-  let orders = EgoStoreService::wallet_order_list(exist_wallet_id).unwrap();
-  assert_eq!(1, orders.len());
+    let ledger_principal = Principal::from_text(LEDGER_ID.to_string()).unwrap();
+    let exist_wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
+    // get order list before make order
+    let orders = EgoStoreService::wallet_order_list(exist_wallet_id).unwrap();
+    assert_eq!(1, orders.len());
 
-  EGO_STORE.with(|ego_store| {
-    assert_eq!(0, ego_store.borrow().wallets.get(&exist_wallet_id).unwrap().cycles);
-  });
+    EGO_STORE.with(|ego_store| {
+        assert_eq!(
+            0,
+            ego_store
+                .borrow()
+                .wallets
+                .get(&exist_wallet_id)
+                .unwrap()
+                .cycles
+        );
+    });
 
-  let order = orders.get(0).unwrap();
-  assert_eq!(OrderStatus::NEW, order.status);
+    let order = orders.get(0).unwrap();
+    assert_eq!(OrderStatus::NEW, order.status);
 
-  // notify order
-  let result = EgoStoreService::wallet_order_notify(order.memo, ledger_principal, 1234567890);
-  assert!(result.is_ok());
+    // notify order
+    let result = EgoStoreService::wallet_order_notify(order.memo, ledger_principal, 1234567890);
+    assert!(result.is_ok());
 
-  // get order list after make order
-  let orders = EgoStoreService::wallet_order_list(exist_wallet_id).unwrap();
-  assert_eq!(1, orders.len());
+    // get order list after make order
+    let orders = EgoStoreService::wallet_order_list(exist_wallet_id).unwrap();
+    assert_eq!(1, orders.len());
 
-  let order = orders.get(0).unwrap();
-  assert_eq!(OrderStatus::SUCCESS, order.status);
+    let order = orders.get(0).unwrap();
+    assert_eq!(OrderStatus::SUCCESS, order.status);
 
-  EGO_STORE.with(|ego_store| {
-    assert_eq!((1.2f32 * 1000000f32) as u128, ego_store.borrow().wallets.get(&exist_wallet_id).unwrap().cycles);
-  });
+    EGO_STORE.with(|ego_store| {
+        assert_eq!(
+            (1.2f32 * 1000000f32) as u128,
+            ego_store
+                .borrow()
+                .wallets
+                .get(&exist_wallet_id)
+                .unwrap()
+                .cycles
+        );
+    });
 }
 
-
 #[test]
-fn wallet_order_list_wallet_not_exists(){
-  // set_up();
-  let test_wallet_id = Principal::from_text(TEST_WALLET_ID).unwrap();
+fn wallet_order_list_wallet_not_exists() {
+    // set_up();
+    let test_wallet_id = Principal::from_text(TEST_WALLET_ID).unwrap();
 
-  //ego store wallet not exists
-  let result = EgoStoreService::wallet_order_list(test_wallet_id);
-  assert!(result.is_err());
-  assert_eq!(3006, result.as_ref().unwrap_err().code);
-  assert_eq!("ego-store: wallet not exists", result.as_ref().unwrap_err().msg);
+    //ego store wallet not exists
+    let result = EgoStoreService::wallet_order_list(test_wallet_id);
+    assert!(result.is_err());
+    assert_eq!(3006, result.as_ref().unwrap_err().code);
+    assert_eq!(
+        "ego-store: wallet not exists",
+        result.as_ref().unwrap_err().msg
+    );
 }
 
 #[test]
-fn wallet_order_new_wallet_not_exists(){
+fn wallet_order_new_wallet_not_exists() {
     // set_up();
     let mut ego_ledger = MockLedger::new();
-    ego_ledger
-      .expect_ledger_payment_add()
-      .returning(|_| ());
+    ego_ledger.expect_ledger_payment_add().returning(|_| ());
     let wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
     let store_id = Principal::from_text(STORE_ID).unwrap();
     // wallet not exists
     let result = EgoStoreService::wallet_order_new(ego_ledger, wallet_id, store_id, 1.5f32);
     assert!(result.is_err());
-    assert_eq!(3006,result.as_ref().unwrap_err().code);
+    assert_eq!(3006, result.as_ref().unwrap_err().code);
 }
 
 #[test]
-fn wallet_cycle_charge_wallet_not_exists(){
-  set_up();
-  let wallet_id = Principal::from_text(TEST_WALLET_ID).unwrap();
-  let ledger_id = Principal::from_text(TEST_OPERATOR).unwrap();
+fn wallet_cycle_charge_wallet_not_exists() {
+    set_up();
+    let wallet_id = Principal::from_text(TEST_WALLET_ID).unwrap();
+    let ledger_id = Principal::from_text(TEST_OPERATOR).unwrap();
 
-  // wallet not exists
-  let result = EgoStoreService::wallet_cycle_charge(wallet_id, 128, ledger_id, 64, "charge cycle".to_string());
-  assert!(result.is_err());
-  assert_eq!(3006, result.as_ref().unwrap_err().code);
-  assert_eq!("ego-store: wallet not exists", result.as_ref().unwrap_err().msg);
+    // wallet not exists
+    let result = EgoStoreService::wallet_cycle_charge(
+        wallet_id,
+        128,
+        ledger_id,
+        64,
+        "charge cycle".to_string(),
+    );
+    assert!(result.is_err());
+    assert_eq!(3006, result.as_ref().unwrap_err().code);
+    assert_eq!(
+        "ego-store: wallet not exists",
+        result.as_ref().unwrap_err().msg
+    );
 }
 
 #[test]
-fn wallet_cycle_charge(){
-  set_up();
-  let wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
-  let ledger_id = Principal::from_text(LEDGER_ID).unwrap();
-  // wallet charge cycle
-  let result = EgoStoreService::wallet_cycle_charge(wallet_id, 128, ledger_id, 64, "charge cycle".to_string());
-  println!("{:?}", result);
+fn wallet_cycle_charge() {
+    set_up();
+    let wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
+    let ledger_id = Principal::from_text(LEDGER_ID).unwrap();
+    // wallet charge cycle
+    let result = EgoStoreService::wallet_cycle_charge(
+        wallet_id,
+        128,
+        ledger_id,
+        64,
+        "charge cycle".to_string(),
+    );
+    assert!(result.is_ok());
 }
--- ego/infra/ego_store/mod/tests/wallet_provider_test.rs
+++ ego/infra/ego_store/mod/tests/wallet_provider_test.rs
@@ -1,12 +1,39 @@
-use ic_cdk::export::Principal;
-
+use ego_store_mod::order::Order;
 use ego_store_mod::service::EgoStoreService;
 use ego_store_mod::state::EGO_STORE;
+use ego_store_mod::wallet::Wallet;
+use ic_cdk::export::Principal;
+use ic_ledger_types::Memo;
 
 static WALLET_PROVIDER_ID: &str = "2265i-mqaaa-aaaad-qbsga-cai";
 static WALLET_APP_ID: &str = "app_exists";
 static EXISTS_WALLET_ID: &str = "23vqh-waaaa-aaaai-qhcya-cai";
-// static TENANT_ID: &str ="2avdy-paaaa-aaaaf-abcga-cai";
+static TENANT_ID: &str = "2avdy-paaaa-aaaaf-abcga-cai";
+static STORE_ID: &str = "22cl3-kqaaa-aaaaf-add7q-cai";
+static EXISTS_USER_ID: &str = "225da-yaaaa-aaaah-qahrq-cai";
+
+pub fn set_up() {
+    let tenant_principal = Principal::from_text(TENANT_ID.to_string()).unwrap();
+    let wallet_principal = Principal::from_text(EXISTS_WALLET_ID.to_string()).unwrap();
+    let user_principal = Principal::from_text(EXISTS_USER_ID.to_string()).unwrap();
+    let store_principal = Principal::from_text(STORE_ID.to_string()).unwrap();
+
+    EGO_STORE.with(|ego_store| {
+        // add wallet
+        let mut wallet = Wallet::new(tenant_principal, wallet_principal, user_principal);
+
+        // add order
+        let order = Order::new(wallet_principal, &store_principal, 12.5f32, 10);
+        ego_store.borrow_mut().orders.insert(Memo(10), order);
+
+        wallet.orders.push(Memo(10));
+
+        ego_store
+            .borrow_mut()
+            .wallets
+            .insert(wallet_principal, wallet);
+    });
+}
 
 #[test]
 fn admin_wallet_provider_add() {
@@ -28,12 +55,63 @@ fn admin_wallet_provider_add() {
 }
 
 #[test]
-fn admin_wallet_cycle_recharge_wallet_not_exists(){
+fn admin_wallet_cycle_recharge_wallet_not_exists() {
     let wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
     let operator = Principal::from_text(WALLET_PROVIDER_ID).unwrap();
-    let result = EgoStoreService::admin_wallet_cycle_recharge(wallet_id, 128, operator, 64, "comment".to_string());
+    let result = EgoStoreService::admin_wallet_cycle_recharge(
+        wallet_id,
+        128,
+        operator,
+        64,
+        "comment".to_string(),
+    );
     assert!(result.is_err());
     assert_eq!(3006, result.as_ref().unwrap_err().code);
-    assert_eq!("ego-store: wallet not exists", result.as_ref().unwrap_err().msg);
+    assert_eq!(
+        "ego-store: wallet not exists",
+        result.as_ref().unwrap_err().msg
+    );
+}
+
+#[test]
+fn admin_wallet_cycle_recharge() {
+    set_up();
+    let wallet_id = Principal::from_text(EXISTS_WALLET_ID).unwrap();
+    let operator = Principal::from_text(EXISTS_USER_ID).unwrap();
+
+    // before recharge
+    let cycle_list = EgoStoreService::wallet_cycle_list(wallet_id);
+    assert_eq!(0, cycle_list.unwrap().len());
+
+    let result = EgoStoreService::admin_wallet_cycle_recharge(
+        wallet_id,
+        128,
+        operator,
+        64,
+        "admin wallet cycle recharge".to_string(),
+    );
+    assert!(result.is_ok());
+
+    // after recharge
+    let cycle_list = EgoStoreService::wallet_cycle_list(wallet_id);
+    println!("{:?}", cycle_list);
+    assert_eq!(1, cycle_list.unwrap().len());
+}
+
+#[test]
+fn admin_ego_tenant_add() {
+    let tenant_id = Principal::from_text(TENANT_ID).unwrap();
+
+    // before add
+    EGO_STORE.with(|ego_store| {
+        assert_eq!(0, ego_store.borrow().tenants.len());
+    });
+
+    let result = EgoStoreService::admin_ego_tenant_add(tenant_id);
     println!("{:?}", result);
+
+    // after add
+    EGO_STORE.with(|ego_store| {
+        assert_eq!(1, ego_store.borrow().tenants.len());
+    });
 }
