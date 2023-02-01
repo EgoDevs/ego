1b26b6b0934752f0d49ca4ebd3beb36d0d252028
邱春季 <qiuchunji01@chinatopcredit.com>
2023-02-01 17:49:36 +0800

    update mod_macro testcase

--- ego/lib/ego_macros/tests/app_info_test.rs
+++ ego/lib/ego_macros/tests/app_info_test.rs
@@ -1,12 +1,13 @@
-use ego_macros::{inject_app_info, inject_cycle_info};
+use ego_macros::{inject_app_info, inject_cycle_info, inject_ego_data};
 use std::cell::RefCell;
-use ic_cdk::export::Principal;
+// use ic_cdk::export::Principal;
 
 inject_app_info!();
 inject_cycle_info!();
-// inject_ego_data!();
+inject_ego_data!();
 
 static WALLET_ID: &str = "amybd-zyaaa-aaaah-qc4hq-cai";
+static EXISTS_CANISTER_ID: &str = "223xb-saaaa-aaaaf-arlqa-cai";
 static APP_ID: &str = "app_exists";
 
 fn set_up(){
@@ -102,6 +103,7 @@ fn cycle_recore_test()
   // before cycle add
   let pre_cycle_info = cycle_info_get();
   assert_eq!(0, pre_cycle_info.estimate_remaining);
+  // println!("pre_cycle_info {:?}", pre_cycle_info);
   
   // before cycle add check cycle record list
   let pre_cycle_record_list = cycle_record_list();
@@ -112,9 +114,11 @@ fn cycle_recore_test()
   assert_eq!((), cycle_record);
 
   // check cycle info after cycle recharge
-  let cycle_info = cycle_info_get();
-  assert_eq!(recharge_num, cycle_info.records[0].balance);
-  assert_eq!(ts, cycle_info.records[0].ts);
+  let cycle_info_1 = cycle_info_get();
+  assert_eq!(recharge_num, cycle_info_1.records[0].balance);
+  assert_eq!(ts, cycle_info_1.records[0].ts);
+  assert_eq!(0, cycle_info_1.estimate_remaining);
+  // println!("cycle_info {:?}", cycle_info_1);
 
   let cycle_record_list = cycle_record_list();
   // assert_eq!(cycle_record_list);
@@ -128,23 +132,25 @@ fn cycle_recore_test()
   // check cycle info pre upgrade
   let cycle_pre_upgrade = cycle_info_pre_upgrade();
   assert_eq!(recharge_num, cycle_pre_upgrade.records[0].balance);
+  assert_eq!(ts, cycle_pre_upgrade.records[0].ts);
   assert_eq!(es_timate, cycle_pre_upgrade.estimate_remaining);
+  // println!("cycle_pre_upgrade {:?}", cycle_pre_upgrade);
 
-  let stable_state = cycle_info_get();
-  println!("{:?}", stable_state);
-  let cycle_post_upgrade = cycle_info_post_upgrade(cycle_info);
-  println!("{:?}", cycle_post_upgrade);
-  let stable_state_new = cycle_info_get();
-  println!("{:?}", stable_state_new);
+  let cycle_post_upgrade = cycle_info_post_upgrade(cycle_pre_upgrade);
+  assert_eq!((), cycle_post_upgrade);
+  // println!("cycle_post_upgrade {:?}", cycle_post_upgrade);
 
-  // let cycle_available = is_cycle_available(12800);
-  // println!("{:?}", cycle_available);
+  // check cycle info after upgrade
+  let cycle_info_2 = cycle_info_get();
+  assert_eq!(recharge_num, cycle_info_2.records[0].balance);
+  assert_eq!(ts, cycle_info_2.records[0].ts);
+  assert_eq!(es_timate, cycle_info_2.estimate_remaining);
 }
 
-// #[test]
-// fn info_log_add_test(){
-//   set_up();
-//   let info_log = info_log_add("cycle recharge add");
-//   println!("{:?}", info_log)
-
-// }
+#[test]
+fn log_list_test(){
+  // set_up();
+  let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
+  let canister_add = canister_add("user1".to_string(), canister_id);
+  println!("canister_add {:?}", canister_add);
+}
