d096c808346ac5b27f3d35e7baab56a9c6c152ec
邱春季 <qiuchunji01@chinatopcredit.com>
2023-02-02 18:30:34 +0800

    update mod macro test case

--- ego/lib/ego_macros/tests/app_info_test.rs
+++ ego/lib/ego_macros/tests/app_info_test.rs
@@ -10,147 +10,226 @@ static WALLET_ID: &str = "amybd-zyaaa-aaaah-qc4hq-cai";
 static EXISTS_CANISTER_ID: &str = "223xb-saaaa-aaaaf-arlqa-cai";
 static APP_ID: &str = "app_exists";
 
-fn set_up(){
-  let version = Version{
-    major: 1,
-    minor: 0,
-    patch: 0
-  };
+fn on_canister_added(_name: &str, _canister_id: Principal) {}
 
-  let wallet_id = Principal::from_text(WALLET_ID.to_string()).unwrap();
+fn set_up() {
+    let version = Version {
+        major: 1,
+        minor: 0,
+        patch: 0,
+    };
 
-  APP_INFO.with(|info| {
-    info.borrow_mut().wallet_id = Some(wallet_id);
-    info.borrow_mut().app_id = APP_ID.to_string();
-    info.borrow_mut().current_version = version;
-    info.borrow_mut().latest_version = version;
-  });
+    let wallet_id = Principal::from_text(WALLET_ID.to_string()).unwrap();
+
+    APP_INFO.with(|info| {
+        info.borrow_mut().wallet_id = Some(wallet_id);
+        info.borrow_mut().app_id = APP_ID.to_string();
+        info.borrow_mut().current_version = version;
+        info.borrow_mut().latest_version = version;
+    });
 }
 
 #[test]
-fn app_info_update_test(){
-  set_up();
+fn app_info_update_test() {
+    set_up();
 
-  let mut app_info = app_info_get();
+    let mut app_info = app_info_get();
 
-  let version = Version{
-    major: 1,
-    minor: 0,
-    patch: 0
-  };
+    let version = Version {
+        major: 1,
+        minor: 0,
+        patch: 0,
+    };
 
-  assert_eq!(APP_ID, app_info.app_id);
-  assert_eq!(version, app_info.current_version);
+    assert_eq!(APP_ID, app_info.app_id);
+    assert_eq!(version, app_info.current_version);
 
-  let new_version = Version{
-    major: 1,
-    minor: 0,
-    patch: 1
-  };
+    let new_version = Version {
+        major: 1,
+        minor: 0,
+        patch: 1,
+    };
 
-  app_info_update(None, APP_ID.to_string(), new_version);
+    app_info_update(None, APP_ID.to_string(), new_version);
 
-  app_info = app_info_get();
-  assert_eq!(APP_ID, app_info.app_id);
-  assert_eq!(new_version, app_info.current_version);
+    app_info = app_info_get();
+    assert_eq!(APP_ID, app_info.app_id);
+    assert_eq!(new_version, app_info.current_version);
 }
 
 #[test]
 fn app_info_get_test() {
-  set_up();
+    set_up();
 
-  let app_info = app_info_get();
+    let app_info = app_info_get();
 
-  let version = Version{
-    major: 1,
-    minor: 0,
-    patch: 0
-  };
+    let version = Version {
+        major: 1,
+        minor: 0,
+        patch: 0,
+    };
 
-  assert_eq!(APP_ID, app_info.app_id);
-  assert_eq!(version, app_info.current_version);
+    assert_eq!(APP_ID, app_info.app_id);
+    assert_eq!(version, app_info.current_version);
 }
 
 #[test]
 fn app_info_pre_upgrade_test() {
-  set_up();
-  let version = Version{
-    major: 1,
-    minor: 0,
-    patch: 0
-  };
+    set_up();
+    let version = Version {
+        major: 1,
+        minor: 0,
+        patch: 0,
+    };
 
-  let app_info = app_info_pre_upgrade();
-  assert_eq!(version, app_info.current_version);
-  assert_eq!(APP_ID, app_info.app_id);
+    let app_info = app_info_pre_upgrade();
+    assert_eq!(version, app_info.current_version);
+    assert_eq!(APP_ID, app_info.app_id);
 }
 
 #[test]
 fn app_info_post_upgrade_test() {
-  set_up();
-  let app1 = app_info_get();
+    set_up();
+    let app1 = app_info_get();
 
-  let app_info = app_info_post_upgrade(app1);
-  assert_eq!((), app_info);
+    let app_info = app_info_post_upgrade(app1);
+    assert_eq!((), app_info);
 }
 
 #[test]
-fn cycle_recore_test()
-{
-  let recharge_num = 864000;
-  let ts = 64;
+fn cycle_record_test() {
+    let recharge_num = 864000;
+    let ts = 64;
 
-  // before cycle add
-  let pre_cycle_info = cycle_info_get();
-  assert_eq!(0, pre_cycle_info.estimate_remaining);
-  // println!("pre_cycle_info {:?}", pre_cycle_info);
-  
-  // before cycle add check cycle record list
-  let pre_cycle_record_list = cycle_record_list();
-  assert_eq!(0, pre_cycle_record_list.len());
+    // before cycle add
+    let pre_cycle_info = cycle_info_get();
+    assert_eq!(0, pre_cycle_info.estimate_remaining);
+    // println!("pre_cycle_info {:?}", pre_cycle_info);
 
-  // cycle recharge
-  let cycle_record= cycle_record_add(recharge_num, ts);
-  assert_eq!((), cycle_record);
+    // before cycle add check cycle record list
+    let pre_cycle_record_list = cycle_record_list();
+    assert_eq!(0, pre_cycle_record_list.len());
 
-  // check cycle info after cycle recharge
-  let cycle_info_1 = cycle_info_get();
-  assert_eq!(recharge_num, cycle_info_1.records[0].balance);
-  assert_eq!(ts, cycle_info_1.records[0].ts);
-  assert_eq!(0, cycle_info_1.estimate_remaining);
-  // println!("cycle_info {:?}", cycle_info_1);
+    // cycle recharge
+    let cycle_record = cycle_record_add(recharge_num, ts);
+    assert_eq!((), cycle_record);
 
-  let cycle_record_list = cycle_record_list();
-  // assert_eq!(cycle_record_list);
-  assert_eq!(1, cycle_record_list.len());
+    // check cycle info after cycle recharge
+    let cycle_info_1 = cycle_info_get();
+    assert_eq!(recharge_num, cycle_info_1.records[0].balance);
+    assert_eq!(ts, cycle_info_1.records[0].ts);
+    assert_eq!(0, cycle_info_1.estimate_remaining);
+    // println!("cycle_info {:?}", cycle_info_1);
 
-  // estimate remaining set
-  let es_timate = 128;
-  let estimate = estimate_remaining_set(es_timate);
-  assert_eq!((), estimate);
+    let cycle_record_list = cycle_record_list();
+    // assert_eq!(cycle_record_list);
+    assert_eq!(1, cycle_record_list.len());
 
-  // check cycle info pre upgrade
-  let cycle_pre_upgrade = cycle_info_pre_upgrade();
-  assert_eq!(recharge_num, cycle_pre_upgrade.records[0].balance);
-  assert_eq!(ts, cycle_pre_upgrade.records[0].ts);
-  assert_eq!(es_timate, cycle_pre_upgrade.estimate_remaining);
-  // println!("cycle_pre_upgrade {:?}", cycle_pre_upgrade);
+    // estimate remaining set
+    let es_timate = 128;
+    let estimate = estimate_remaining_set(es_timate);
+    assert_eq!((), estimate);
 
-  let cycle_post_upgrade = cycle_info_post_upgrade(cycle_pre_upgrade);
-  assert_eq!((), cycle_post_upgrade);
-  // println!("cycle_post_upgrade {:?}", cycle_post_upgrade);
+    // check cycle info pre upgrade
+    let cycle_pre_upgrade = cycle_info_pre_upgrade();
+    assert_eq!(recharge_num, cycle_pre_upgrade.records[0].balance);
+    assert_eq!(ts, cycle_pre_upgrade.records[0].ts);
+    assert_eq!(es_timate, cycle_pre_upgrade.estimate_remaining);
+    // println!("cycle_pre_upgrade {:?}", cycle_pre_upgrade);
 
-  // check cycle info after upgrade
-  let cycle_info_2 = cycle_info_get();
-  assert_eq!(recharge_num, cycle_info_2.records[0].balance);
-  assert_eq!(ts, cycle_info_2.records[0].ts);
-  assert_eq!(es_timate, cycle_info_2.estimate_remaining);
+    let cycle_post_upgrade = cycle_info_post_upgrade(cycle_pre_upgrade);
+    assert_eq!((), cycle_post_upgrade);
+    // println!("cycle_post_upgrade {:?}", cycle_post_upgrade);
+
+    // check cycle info after upgrade
+    let cycle_info_2 = cycle_info_get();
+    assert_eq!(recharge_num, cycle_info_2.records[0].balance);
+    assert_eq!(ts, cycle_info_2.records[0].ts);
+    assert_eq!(es_timate, cycle_info_2.estimate_remaining);
 }
 
 #[test]
-fn log_list_test(){
-  // set_up();
-  let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
-  let canister_add = canister_add("user1".to_string(), canister_id);
-  println!("canister_add {:?}", canister_add);
+fn cnaister_add_test() {
+    // get canister list befor canister add
+    let pre_canister_list = canister_list();
+    assert!(pre_canister_list.is_empty());
+
+    // canister add
+    let name = "user1";
+    let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
+    let _canister_add = canister_add(name.to_string(), canister_id);
+    assert_eq!((), _canister_add);
+
+    // get canister list after canister add
+    let after_canister_list = canister_list();
+    assert_eq!(1, after_canister_list.len());
+    assert!(after_canister_list.contains_key(name));
+}
+
+#[test]
+fn canister_remove_test() {
+    // canister add
+    let name = "user1";
+    let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
+    let _canister_add = canister_add(name.to_string(), canister_id);
+    assert_eq!((), _canister_add);
+
+    // remove canister
+    let canister_remove = canister_remove(name.to_string(), canister_id);
+    assert_eq!((), canister_remove);
+
+    // get canister list after canister remove
+    let remove_canister_list = canister_list();
+    assert!(remove_canister_list.contains_key(name));
+}
+
+#[test]
+fn canister_remove_all_test() {
+    // canister add
+    let name = "user1";
+    let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
+    let _canister_add = canister_add(name.to_string(), canister_id);
+    assert_eq!((), _canister_add);
+
+    // remove canister
+    let canister_remove_all = canister_remove_all(name.to_string());
+    assert_eq!((), canister_remove_all);
+
+    // get canister list after canister remove
+    let remove_canister_list = canister_list();
+    assert!(remove_canister_list.is_empty());
+}
+
+#[test]
+fn canister_get_one_test() {
+    // canister get one befor canister add
+    let get_one = canister_get_one("user1");
+    assert!(get_one.is_none());
+
+    // canister add
+    let name = "user1";
+    let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
+    let _canister_add = canister_add(name.to_string(), canister_id);
+    assert_eq!((), _canister_add);
+
+    // canister get one after canister added
+    let get_one_1 = canister_get_one("user1");
+    assert!(get_one_1.is_some());
+}
+
+#[test]
+fn canister_get_all_test() {
+    // canister get all before canister add
+    // let get_all = canister_get_all("user1");
+    // println!("get_all {:?}", get_all);
+
+    // canister add
+    let name = "user1";
+    let canister_id = Principal::from_text(EXISTS_CANISTER_ID.to_string()).unwrap();
+    let _canister_add = canister_add(name.to_string(), canister_id);
+    assert_eq!((), _canister_add);
+
+    // canister get one after canister added
+    let get_all_1 = canister_get_all("user1");
+    assert!(!get_all_1.is_empty());
 }
